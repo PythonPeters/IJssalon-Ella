94d2fe35c036297cb4fd56064afb3ce36d456a8e
peter de reu <peter.dereu@telenet.be>
2024-06-10 14:40:58 +0200

    bestand tijdelijk.py gecodeerd

---
+++ tijdelijk.py
@@ -0,0 +1,16 @@
+prijzen = {
+    "aardbei" : "3",
+    "vanille" : "4",
+    "chocolade" : "5",
+}
+aanbieding = (prijzen["aardbei"])
+aanbieding = int(aanbieding) * 0.8
+reclame_tekst1 = (f"Vandaag in de aanbieding : aardbei-ijs, 1 liter is slechts €{aanbieding}.")
+reclame_tekst2 = reclame_tekst1[:64]
+reclame_tekst3 = reclame_tekst2.upper()
+reclame_tekst4 = reclame_tekst3.split()
+for el in reclame_tekst4:
+    if (len(el)) >= 5:
+        print(el)
+    else:
+       print(el.lower())
